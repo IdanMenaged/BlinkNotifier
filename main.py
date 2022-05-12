39b961934f8ed1a769de35339e47de7dad9899f8
IdanMenaged <102684625+IdanMenaged@users.noreply.github.com>
2022-05-12 10:31:10 +0300

    get individual frame complete

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import cv2
 
 def main():
-    cv2.imshow('frame', Camera().get_frame())
-    Camera().take_video()
+    while cv2.waitKey(1) != 27:
+        cv2.imshow('winname', Camera().get_frame())
 
 class Camera:    
     def __init__(self, *args):
         super(Camera, self).__init__(*args)
         
-    def get_frame(self):
-        """get a single frame
-
-        Raises:
-            Exception: when camera does not open
-
-        Returns:
-            numpy_array: representing the image, can use cv2.imshow(winname, mat) with met as the array to display the image
-        """
-        cap = cv2.VideoCapture(0)  # VideoCapture takes 0 for the default camera
-        ret, frame = cap.read()
-        
-        if not cap.isOpened() or not ret:  # makes sure the camera was opened
+        self.cap = cv2.VideoCapture(0)
+        if not self.cap.isOpened():  # makes sure the camera was opened
             raise Exception('Could not open camera. Try to change your default camera?')
         
-        return frame
+    def get_frame(self, device_index=0):
+        """records a single frame
+        put inside a `while cv2.waitKey(1) != 27` to record until the ESC key is pressed
+        change 27 to any ascii value to change the key press needed
+
+        Args:
+            device_index (int, optional): represents the device index of input camera. Defaults to 0 (the default camera).
+
+        Raises:
+            Exception: when the program cannot record (ret == False)
+
+        Returns:
+            numpy.array: representing the frame
+        """
+        cap = cv2.VideoCapture(device_index)
+            
+        ret, frame = cap.read()  # ret == True if the camera is recording, frame == numpy.array representing the frame
+        if ret:
+            return frame
+        else:
+            raise Exception('Could not record')
     
     def take_video(self):
         """take video until ESC key is pressed
