35dc79dd979ed046e2882f8e7cbdcc872ef0efcc
IdanMenaged <102684625+IdanMenaged@users.noreply.github.com>
2022-05-15 23:20:21 +0300

    can take images and detect eyes

--- camera.py
+++ camera.py
@@ -3,19 +3,13 @@ import cv2
 def main():
     while cv2.waitKey(1) != 27:
         cv2.imshow('winname', Camera().get_frame())
-
+    
 class Camera:    
     def __init__(self, *args):
         super(Camera, self).__init__(*args)
         
-        self.cap = cv2.VideoCapture(0)
-        if not self.cap.isOpened():  # makes sure the camera was opened
-            raise Exception('Could not open camera. Try to change your default camera?')
-        
     def get_frame(self, device_index=0):
         """records a single frame
-        put inside a `while cv2.waitKey(1) != 27` to record until the ESC key is pressed
-        change 27 to any ascii value to change the key press needed
 
         Args:
             device_index (int, optional): represents the device index of input camera. Defaults to 0 (the default camera).
@@ -27,12 +21,26 @@ class Camera:
             numpy.array: representing the frame
         """
         cap = cv2.VideoCapture(device_index)
+        if not cap.isOpened():  # makes sure the camera was opened
+            raise Exception('Could not open camera. Try to change your default camera?')
             
         ret, frame = cap.read()  # ret == True if the camera is recording, frame == numpy.array representing the frame
         if ret:
+            cap.release()
             return frame
         else:
             raise Exception('Could not record')
+        
+    def show_frame(self, frame, winname, waitkey=27):
+        """displays a given frame
+
+        Args:
+            frame (numpy.array): returned by the Camera.get_frame(self) method
+            winname (string): the name of the window
+            waitkey (int, optional): an ascii value representing a key. the image is displayed until said key is pressed. Defaults to 27 (ESC).
+        """
+        while cv2.waitKey(1) != waitkey:
+            cv2.imshow(winname, frame)
     
     def take_video(self):
         """take video until ESC key is pressed
---
+++ recognizer.py
@@ -0,0 +1,48 @@
+import cv2
+
+def main():
+    from camera import Camera
+    img = Camera().get_frame()
+    eyes = Recognizer().detect_eyes(img)
+    Recognizer().draw_rectangle(eyes, img, winname='eyes')
+
+class Recognizer:    
+    def __init__(self, *args):
+        super(Recognizer, self).__init__(*args)
+        
+    # TO DO: try to generelize it so that it can detect objects in general, not just eyes
+    def detect_eyes(self, img):
+        """detects all eyes in a given image
+
+        Args:
+            img (numpy.array): the image you wish to process
+
+        Returns:
+            tuple: representing a rectangle around the found eyes. the tuple consists of (x, y, w, h) where x and y are the top left coordinates while w and h are the width and the height of the rectangle
+        """
+        eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
+        eyes = eye_cascade.detectMultiScale(gray, 1.3, 5)
+        return eyes
+    
+    # TO DO: seperate adding the rectangle and displaying the image
+    def draw_rectangle(self, obj, img, color=(0, 255, 0), thickness=2, winname='image'):
+        """adds a rectangle around an object in a given img
+
+        Args:
+            obj (tuple or list of tuples): consists of (x, y, w, h) representing x y coordinates, width, and height. can be multiple tuples
+            img (numpy.array): the image you wish to add rectangles to
+            color (tuple, optional): RGB value representing the color of the rectangole. Defaults to (0, 255, 0) which is green.
+            thickness (int, optional): controls how thick the outline is. Defaults to 2.
+            winname (str, optional): controls the name of the window in which the image is drawn. Defaults to 'image'.
+        """
+        for (x,y,w,h) in obj:
+            img = cv2.rectangle(img,(x,y),(x+w,y+h),color,thickness)
+            
+        cv2.imshow(winname, img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+if __name__ == '__main__':
+    main()
